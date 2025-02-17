2f7d5c3d68b2aeb01ce0f7d5a35635c12e195bf9
yublee71 <lybin71@naver.com>
2025-02-17 19:04:38 +0000

    feat: add function prototypes

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,14 +1,5 @@
 #include "Account.hpp"
 
-Account::Account(int initial_deposit)
-{
-	this->_amount = initial_deposit;
-}
-
-Account::~Account(void)
-{
-}
-
 int Account::getNbAccounts(void)
 {
 	return (Account::_nbAccounts);
@@ -18,16 +9,43 @@ int Account::getTotalAmount(void)
 {
 	return (Account::_totalAmount);
 }
+
 int Account::getNbDeposits(void)
 {
 	return (Account::_totalNbDeposits);
 }
+
 int Account::getNbWithdrawals(void)
 {
 	return (Account::_totalNbWithdrawals);
 }
 
 void Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+}
+
+Account::~Account(void)
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+}
+
+int Account::checkAmount(void) const
+{
+}
+
+void Account::displayStatus(void) const
 {
 }
 
