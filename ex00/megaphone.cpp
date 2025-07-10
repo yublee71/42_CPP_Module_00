09ec42ddeb875427926a47e673ee2e096c848aa4
Yubeen Lee <lybin71@naver.com>
2025-07-10 18:20:36 +0100

    style: use clang formatter

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
 #include <cctype>
+#include <iostream>
 
-int main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)toupper(argv[i][j]);
-		}
-	}
-	std::cout << std::endl;
+int main(int argc, char **argv) {
+  if (argc == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  else {
+    for (int i = 1; i < argc; i++) {
+      for (int j = 0; argv[i][j]; j++)
+        std::cout << (char)std::toupper(argv[i][j]);
+    }
+  }
+  std::cout << std::endl;
 }
